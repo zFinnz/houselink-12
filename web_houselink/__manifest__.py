f2478536dc4b2e207a6bb081c89bab1609fc7879
linh.nguyenphuc <linhutt@gmail.com>
2021-08-02 18:27:42 +0700

    init project

---
+++ web_houselink/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Web houselink',
+    'version': '12.0.1.1',
+    'summary': """Web Houselink""",
+    'author': 'Houselink',
+    'category': 'Productivity',
+    'license': 'GPL-3',
+    'website': 'https://houselink.com.vn',
+    'description': """
+""",
+    'depends': ['base', 'web'],
+    'images': [],
+    'data': [
+        'views/assets_backend.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
